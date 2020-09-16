662dcbf24a50e32136b0c98bf14b63f134d1f01f
sonikku10 <sonikku10@msn.com>
2020-09-16 17:43:51 -0400

    Implement EJS

--- app.js
+++ app.js
@@ -6,21 +6,20 @@ const bodyPar = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req,res) {
 
     var today = new Date();
     var currentDay = today.getDay()
+    var day = "";
 
     if (currentDay === 6 || currentDay === 0) {
-      res.write("<h1>Yay it's the weekend!</h1>");
+      day = "weekend";
     } else {
-
-
-      res.sendFile(__dirname + "/index.html");
+      day = "weekday";
     }
-
-
-
+    res.render("list", {kindOfDay: day});
 });
 
 
