e1c6ecad6e482c71812615702bbf77cf10b15d6a
sonikku10 <sonikku10@msn.com>
2020-09-22 16:30:45 -0400

    Add for loop to allow list items to be added upon POST

--- app.js
+++ app.js
@@ -2,21 +2,38 @@
 
 
 const express = require("express");
-const bodyPar = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
 app.get("/", function (req,res) {
 
     var today = new Date();
-    var currentDay = today.getDay()
-    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var dayOfWeek = day[currentDay];
+    var options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+
+    };
+    var day = today.toLocaleDateString("en-US", options);
 
 
-    res.render("list", {kindOfDay: dayOfWeek});
+    res.render("list", {kindOfDay: day, newListItems: items});
+
+    app.post("/", function(req, res) {
+      var item = req.body.newItem
+      items.push(item);
+      res.redirect("/"); //loops back to line 13 app.get
+
+    });
+
+
 });
 
 
