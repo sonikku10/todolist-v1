103002a01fd19bfb8e29efedd5d0a2f4e4e0e5d6
sonikku10 <sonikku10@msn.com>
2020-09-22 18:05:25 -0400

    Refactor code; export getDate to separate module

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,30 +16,32 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req,res) {
 
-    var today = new Date();
-    var options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-
-    };
-    var day = today.toLocaleDateString("en-US", options);
+const day = date.getDate();
 
 
-    res.render("list", {kindOfDay: day, newListItems: items});
-
-    app.post("/", function(req, res) {
-      var item = req.body.newItem
-      items.push(item);
-      res.redirect("/"); //loops back to line 13 app.get
-
-    });
-
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
+    app.post("/", function(req, res) {
+      const item = req.body.newItem;
+      if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+      } else {
+        items.push(item);
+        res.redirect("/");
+      }
+    });
 
 
+app.get ("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
 
 app.listen(3000, function(){
